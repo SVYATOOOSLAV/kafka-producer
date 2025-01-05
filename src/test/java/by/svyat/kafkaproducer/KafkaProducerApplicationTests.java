647fec6df21e96f6c96895f72a1e5ff4cb228714
Svyatoslav Milkin <svyatoslavmilkin@yandex.ru>
2025-01-05 19:33:17 +0300

    Optimize test

--- src/test/java/by/svyat/kafkaproducer/KafkaProducerApplicationTests.java
+++ src/test/java/by/svyat/kafkaproducer/KafkaProducerApplicationTests.java
@@ -2,9 +2,14 @@ package by.svyat.kafkaproducer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaAdmin;
 
 @SpringBootTest
 class KafkaProducerApplicationTests {
+
+	@MockBean
+	private KafkaAdmin kafkaAdmin;
 
 	@Test
 	void contextLoads() {
